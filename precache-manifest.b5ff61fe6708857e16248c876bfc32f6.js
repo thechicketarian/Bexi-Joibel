612df343a37785ef4c83277614a2ba3fac28f372
thechicketarian <thechicketarian@gmail.com>
2022-08-10 20:20:57 -0500

    Updates

---
+++ precache-manifest.b5ff61fe6708857e16248c876bfc32f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "370fe62860c0865cff81",
+    "url": "/React-Porftolio/static/css/main.22696f2d.chunk.css"
+  },
+  {
+    "revision": "370fe62860c0865cff81",
+    "url": "/React-Porftolio/static/js/main.d45be686.chunk.js"
+  },
+  {
+    "revision": "b0d02ec95e589045d6cb",
+    "url": "/React-Porftolio/static/js/runtime~main.ba50c773.js"
+  },
+  {
+    "revision": "2ea4fb25b502d0c4500a",
+    "url": "/React-Porftolio/static/js/2.cfff772e.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/React-Porftolio/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "c35c321aec458e963c029bdc15e8ceb1",
+    "url": "/React-Porftolio/static/media/bexi.c35c321a.jpg"
+  },
+  {
+    "revision": "dd73ac8b51b7606f022e312e0ffcf30e",
+    "url": "/React-Porftolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1f26ee218a5d8dae516292380dc5e020.js"
+  "/React-Porftolio/precache-manifest.b5ff61fe6708857e16248c876bfc32f6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/React-Porftolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
