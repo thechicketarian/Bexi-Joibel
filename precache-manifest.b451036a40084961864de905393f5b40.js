5c77e56f538abfb6a39e7d77fb8cc86b28b906b3
thechicketarian <thechicketarian@gmail.com>
2023-01-31 17:18:27 -0600

    Updates

---
+++ precache-manifest.b451036a40084961864de905393f5b40.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa6efcc269d188a30802",
+    "url": "/BexiJoibel/static/css/main.54ccdc6e.chunk.css"
+  },
+  {
+    "revision": "fa6efcc269d188a30802",
+    "url": "/BexiJoibel/static/js/main.e78df956.chunk.js"
+  },
+  {
+    "revision": "663786df02d33458450a",
+    "url": "/BexiJoibel/static/js/runtime~main.d065707e.js"
+  },
+  {
+    "revision": "721e75b4682d0256cf18",
+    "url": "/BexiJoibel/static/js/2.4eac9804.chunk.js"
+  },
+  {
+    "revision": "bb895545e484ff0164d4",
+    "url": "/BexiJoibel/static/js/3.71a42320.chunk.js"
+  },
+  {
+    "revision": "a785fef8de1f0ec342901003579551d7",
+    "url": "/BexiJoibel/static/media/BexiResume.a785fef8.pdf"
+  },
+  {
+    "revision": "129c0c7fbb4c0a1267c736079e51d9d0",
+    "url": "/BexiJoibel/static/media/bjlogo.129c0c7f.svg"
+  },
+  {
+    "revision": "3562226b4c74b1c92319e7f689e4d337",
+    "url": "/BexiJoibel/static/media/header.3562226b.mp4"
+  },
+  {
+    "revision": "1d6579770e38c8c8a60d77f3e4f5c875",
+    "url": "/BexiJoibel/static/media/vidthumb.1d657977.jpg"
+  },
+  {
+    "revision": "a2f553f3e3c4e0956e5ca7729b8ebb3e",
+    "url": "/BexiJoibel/static/media/gymRats.a2f553f3.jpg"
+  },
+  {
+    "revision": "2b67ae29ad00789f9c3ee310d203ded9",
+    "url": "/BexiJoibel/static/media/artImg.2b67ae29.jpg"
+  },
+  {
+    "revision": "8002d1edbc976ee7ae62342515aaa8d1",
+    "url": "/BexiJoibel/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d162c92d3c18b1ea2652d7c0c8faac78.js"
+  "/BexiJoibel/precache-manifest.b451036a40084961864de905393f5b40.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/BexiJoibel/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
