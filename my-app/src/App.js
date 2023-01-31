79ddcd9eddaf31a0aee71da1aaa93460e47888ba
thechicketarian <thechicketarian@gmail.com>
2023-01-31 16:27:37 -0600

    another deployment test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -23,7 +23,7 @@ import './index.css';
 function App() {
 
   return (
-    <BrowserRouter basename="React-Portfolio"> 
+    <BrowserRouter basename="/"> 
     <Header/>
     <Routes> 
     <Route path="/" element={<Home/>}/>
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -17,10 +17,10 @@ function Header() {
       
         <div className="navLinks">
           <Link to ="about"> About</Link>
-          <Link to ="design">Design</Link>
+          {/* <Link to ="design">Design</Link> */}
           <Link to ="dev">Dev</Link>
           <Link to ="resume">Resume</Link>
-          <Link to ="contact">Contact</Link>
+          {/* <Link to ="contact">Contact</Link> */}
         </div>
     </nav>
   );
