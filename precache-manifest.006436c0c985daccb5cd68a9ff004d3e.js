411cf3f3c2f87ec5ad87e157ceed1b529e1d98e0
thechicketarian <thechicketarian@gmail.com>
2024-02-06 20:54:49 -0600

    Updates

---
+++ precache-manifest.006436c0c985daccb5cd68a9ff004d3e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27561ca219f25fb14927a3811172f3e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68e06f420dbbf7ff6ddb",
+    "url": "/static/css/main.e2b9cc1e.chunk.css"
+  },
+  {
+    "revision": "991975cc3ad01f2ccdb7",
+    "url": "/static/js/2.3ea34e93.chunk.js"
+  },
+  {
+    "revision": "5099652deffd979d970e",
+    "url": "/static/js/3.d5f86460.chunk.js"
+  },
+  {
+    "revision": "68e06f420dbbf7ff6ddb",
+    "url": "/static/js/main.064d0cf5.chunk.js"
+  },
+  {
+    "revision": "b3d99bc629c5a9184875",
+    "url": "/static/js/runtime~main.2091b4db.js"
+  },
+  {
+    "revision": "a785fef8de1f0ec342901003579551d7",
+    "url": "/static/media/BexiResume.a785fef8.pdf"
+  },
+  {
+    "revision": "e423e36a259076b719eedf1812b94190",
+    "url": "/static/media/artImg.e423e36a.jpg"
+  },
+  {
+    "revision": "129c0c7fbb4c0a1267c736079e51d9d0",
+    "url": "/static/media/bjlogo.129c0c7f.svg"
+  },
+  {
+    "revision": "a2f553f3e3c4e0956e5ca7729b8ebb3e",
+    "url": "/static/media/gymRats.a2f553f3.jpg"
+  },
+  {
+    "revision": "3562226b4c74b1c92319e7f689e4d337",
+    "url": "/static/media/header.3562226b.mp4"
+  },
+  {
+    "revision": "6e9ad8a173d048db249f4d9669fc9cf7",
+    "url": "/static/media/videoMobile.6e9ad8a1.gif"
+  },
+  {
+    "revision": "1d6579770e38c8c8a60d77f3e4f5c875",
+    "url": "/static/media/vidthumb.1d657977.jpg"
+  }
+]);
